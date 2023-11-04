c7acf48344ddd8e1d66b5c0f316e8406618d69b9
ViceAguilera <vicente.aguilera2001@alumnos.ubiobio.cl>
2023-11-04 13:46:01 -0300

    Fix

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Se carga el modelo
-model = YOLO("yolov8n.yaml", device='gpu')  # Se crear un nuevo modelo con Yolo nano
+model = YOLO("yolov8n.pt")  # Se crear un nuevo modelo con Yolo nano
 
 # Use the model
-model.train(data="config.yaml", epochs=5)  # Se entrena el modelo por 3 ciclos
+model.train(data="config.yaml", epochs=5, imgsz=640, batch=8)  # Se entrena el modelo por 3 ciclos
